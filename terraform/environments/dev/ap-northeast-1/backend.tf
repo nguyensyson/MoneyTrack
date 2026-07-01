904c10c774307dcd4c2596dc438e9b0221ecf99e
nguyensyson <ssnguyenson5671@gmail.com>
2026-07-01 22:41:27 +0700

    commit fix backend terraform

--- terraform/environments/dev/ap-northeast-1/backend.tf
+++ terraform/environments/dev/ap-northeast-1/backend.tf
@@ -1,20 +1,9 @@
-# Backend configuration for dev/ap-northeast-1
-#
-# S3 bucket name and DynamoDB table name are NOT hard-coded here.
-# Supply them at init time using a backend.hcl partial config:
-#
-#   terraform init -backend-config=../../../backend.hcl
-#
-# Where backend.hcl contains:
-#   bucket         = "<output from terraform/bootstrap>"
-#   dynamodb_table = "<output from terraform/bootstrap>"
-#   region         = "ap-northeast-1"
-#
-# See terraform/MIGRATION.md for full bootstrap + migration instructions.
-
 terraform {
   backend "s3" {
-    key     = "dev/ap-northeast-1/terraform.tfstate"
-    encrypt = true
+    bucket         = "moneytrack-terraform-state"
+    dynamodb_table = "moneytrack-terraform-locks"
+    region         = "ap-southeast-1"
+    key            = "dev/ap-northeast-1/terraform.tfstate"
+    encrypt        = true
   }
 }
--- terraform/environments/prod/ap-southeast-1/backend.tf
+++ terraform/environments/prod/ap-southeast-1/backend.tf
@@ -1,11 +1,9 @@
 terraform {
   backend "s3" {
-    # Values supplied via: terraform init -backend-config=../../../backend.hcl
-    # backend.hcl should contain:
-    #   bucket         = "<bootstrap output: state_bucket_name>"
-    #   dynamodb_table = "<bootstrap output: lock_table_name>"
-    #   region         = "ap-southeast-1"
-    key     = "prod/ap-southeast-1/terraform.tfstate"
-    encrypt = true
+    bucket         = "moneytrack-terraform-state"
+    dynamodb_table = "moneytrack-terraform-locks"
+    region         = "ap-southeast-1"
+    key            = "prod/ap-southeast-1/terraform.tfstate"
+    encrypt        = true
   }
 }
