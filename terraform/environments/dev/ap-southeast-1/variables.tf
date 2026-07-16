a2bd3ba2a5c72fbefcab63e9fd5195b801ad5482
nguyensyson <ssnguyenson5671@gmail.com>
2026-07-17 00:53:01 +0700

    fix ci cd

--- terraform/environments/dev/ap-southeast-1/variables.tf
+++ terraform/environments/dev/ap-southeast-1/variables.tf
@@ -29,28 +29,31 @@ variable "secondary_region" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets (one per AZ)"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets (one per AZ)"
   type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "domain_name" {
   description = "Root domain name (e.g. moneytrack.com)"
   type        = string
-  default     = "moneytrack.com"
+  default     = "sonns.cloud"
 }
 
 variable "api_subdomain" {
   description = "Subdomain for the backend API (e.g. api)"
   type        = string
-  default     = "api"
+  default     = "money"
 }
 
 variable "create_route53_zone" {
@@ -74,26 +77,31 @@ variable "backend_image_tag" {
 variable "ecs_cpu" {
   description = "CPU units for the ECS Fargate task"
   type        = number
+  default     = 256
 }
 
 variable "ecs_memory" {
   description = "Memory (MiB) for the ECS Fargate task"
   type        = number
+  default     = 512
 }
 
 variable "ecs_desired_count" {
   description = "Desired number of ECS task instances"
   type        = number
+  default     = 1
 }
 
 variable "ecs_min_capacity" {
   description = "Minimum number of ECS tasks for auto-scaling"
   type        = number
+  default     = 1
 }
 
 variable "ecs_max_capacity" {
   description = "Maximum number of ECS tasks for auto-scaling"
   type        = number
+  default     = 2
 }
 
 variable "container_port" {
@@ -105,6 +113,7 @@ variable "container_port" {
 variable "log_retention_days" {
   description = "CloudWatch log group retention in days"
   type        = number
+  default     = 7
 }
 
 variable "dynamodb_table_users" {
@@ -146,13 +155,13 @@ variable "waf_rate_limit" {
 variable "github_org" {
   description = "GitHub organization or username"
   type        = string
-  default     = "moneytrack"
+  default     = "nguyensyson"
 }
 
 variable "github_repo" {
   description = "GitHub repository name"
   type        = string
-  default     = "MoneyTrack_BE"
+  default     = "MoneyTrack"
 }
 
 variable "create_github_oidc_provider" {
